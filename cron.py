21d92a0f4da3bcdb1d2dbacaadc408bcd21e6738
Hasibur Rahman Omi <hasib.omi@cimplux.com>
2021-01-27 14:22:26 +0600

    Seeding & cron job process have been improved and bugs have been fixed

--- cron.py
+++ cron.py
@@ -1,21 +1,80 @@
-import datetime, re
-from mysql.connector import connect
-from angaza import Angaza
+import datetime
+from processes.db import Database
+from processes.helper import Helper
+from processes.vars import ANGAZA_DATA_TYPES
+
 
 query_params = []
+daily_usage_data_query_params = []
+db = Database()
+helper = Helper()
+today = datetime.date.today()
+delta = datetime.timedelta(days=1)
+from_date = today - delta
+unit_numbers = db.get_unitnumbers()
 
-def get_usage_data(unit_number: int, from_when_dt: str, offset: int = 0):
-    data = angaza.get_usage_data(
-        unit_number=unit_number,
-        from_when_dt=from_when_dt,
-        offset=offset
+# Iterate all the unit numbers to get data against a unit number.
+for unit_number in unit_numbers:
+    data = list()
+    daily_usage_data_unorganized = list()
+    daily_usage_data = {}
+    last_usage_data = db.get_usagedata(
+        unit_number=str(unit_number['unit_number']),
+        data_type='228',
+        order_by='when_datetime',
+        descending=True,
+        limit=1
     )
 
-    if '_embedded' in data and len(data['_embedded']['item']) > 0:
-        for item in data['_embedded']['item']:
+    # Calling API to Angaza.
+    helper. get_usage_data(
+        unit_number=unit_number['unit_number'],
+        from_when_dt='{}T00:00:00+00:00'.format(from_date),
+        variable=data
+    )
+
+    if len(data) > 0:
+        data = sorted(
+            data,
+            key=lambda item: item['when']
+        )
+
+        # Convert unreadable data from angaza to readable data.
+        for item in data:
+            when_date = helper.convert_to_datetime(item['when']).date()
+
+            temp_data = {
+                'when_date': str(when_date)
+            }
+            temp_data[ANGAZA_DATA_TYPES[item['type']]] = 0
+
+            for jitem in data:
+                if item['type'] == jitem['type']:
+                    if item['type'] == 228:
+                        temp_data[ANGAZA_DATA_TYPES[item['type']]] = jitem['value']
+                    elif when_date == helper.convert_to_datetime(jitem['when']).date():
+                        temp_data[ANGAZA_DATA_TYPES[item['type']]] += jitem['value']
+
+            daily_usage_data_unorganized.append(temp_data)
+        
+        # Get all data for the same date.
+        for item in daily_usage_data_unorganized:
+            keys = item.keys()
+
+            for i in keys:
+                daily_usage_data[i] = item[i]
+
+        # Stove on/off calculation.
+        if 'stove_on_off_count' in daily_usage_data:
+
+            if last_usage_data:
+                daily_usage_data['stove_on_off_count'] = daily_usage_data['stove_on_off_count'] - float(last_usage_data[0]['data_value'])
+        
+        # Prepare data to insert in to the DB.
+        for item in data:
             query_params.append(
                 (
-                    unit_number,
+                    unit_number['unit_number'],
                     datetime.datetime.strptime(item['when'], '%Y-%m-%dT%H:%M:%Sz'),
                     item['type'],
                     item['value'],
@@ -24,54 +83,43 @@ def get_usage_data(unit_number: int, from_when_dt: str, offset: int = 0):
                 )
             )
 
-        if '_links' in data and 'next' in data['_links']:
-            link = data['_links']['next']['href']
-            offset = re.search('&offset=(.*)', link)
+        daily_usage_data['daily_power_consumption'] = daily_usage_data['daily_power_consumption'] / 1000 if daily_usage_data['daily_power_consumption'] > 0 else daily_usage_data['daily_power_consumption']
+        daily_usage_data['average_power_consumption_per_use'] = 0
+        daily_usage_data['average_cooking_time_per_use'] = 0
 
-            if offset and offset > 0:
-                get_usage_data(unit_number=unit_number, from_when_dt=from_when_dt, offset=offset)
+        daily_usage_data['daily_cooking_time'] = daily_usage_data['left_stove_cooktime'] + daily_usage_data['right_stove_cooktime']
 
-DB_HOST = 'localhost'
-DB_PORT = 3306
-DB_USER = 'root'
-DB_PASSWORD = 'root'
-DB_NAME = 'db_integration_angaza_data'
+        if 'stove_on_off_count' in daily_usage_data and daily_usage_data['stove_on_off_count'] > 0:
+            daily_usage_data['average_power_consumption_per_use'] = daily_usage_data['daily_power_consumption'] / daily_usage_data['stove_on_off_count']
+            daily_usage_data['average_cooking_time_per_use'] = daily_usage_data['daily_cooking_time'] / daily_usage_data['stove_on_off_count']
 
-today = datetime.date.today()
-delta = datetime.timedelta(days=1)
-from_date = today - delta
-angaza = Angaza()
-db = connect(
-    host=DB_HOST,
-    port=DB_PORT,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
-select_cursor = db.cursor(dictionary=True)
-query = 'INSERT INTO reports_usagedata (unit_number, when_datetime, data_type, data_value, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s)'
+        daily_usage_data_query_params.append(
+            (
+                unit_number['unit_number'],
+                daily_usage_data['average_battery_voltage'] if 'average_battery_voltage' in daily_usage_data else 0.00,
+                daily_usage_data['max_battery_voltage'] if 'max_battery_voltage' in daily_usage_data else 0.00,
+                daily_usage_data['min_battery_voltage'] if 'min_battery_voltage' in daily_usage_data else 0.00,
+                daily_usage_data['battery_critical_state_count'] if 'battery_critical_state_count' in daily_usage_data else 0.00,
+                daily_usage_data['one_hour_samples_count'] if 'one_hour_samples_count' in daily_usage_data else 0.00,
+                daily_usage_data['lock_mode_entry_count'] if 'lock_mode_entry_count' in daily_usage_data else 0.00,
+                daily_usage_data['factory_mode_entry_count'] if 'factory_mode_entry_count' in daily_usage_data else 0.00,
+                daily_usage_data['gsm_sync_button_count'] if 'gsm_sync_button_count' in daily_usage_data else 0.00,
+                daily_usage_data['stove_on_off_count'] if 'stove_on_off_count' in daily_usage_data else 0.00,
+                daily_usage_data['daily_power_consumption'] if 'daily_power_consumption' in daily_usage_data else 0.00,
+                daily_usage_data['left_stove_cooktime'] if 'left_stove_cooktime' in daily_usage_data else 0.00,
+                daily_usage_data['right_stove_cooktime'] if 'right_stove_cooktime' in daily_usage_data else 0.00,
+                daily_usage_data['daily_cooking_time'] if 'daily_cooking_time' in daily_usage_data else 0.00,
+                daily_usage_data['average_power_consumption_per_use'] if 'average_power_consumption_per_use' in daily_usage_data else 0.00,
+                daily_usage_data['average_cooking_time_per_use'] if 'average_cooking_time_per_use' in daily_usage_data else 0.00,
+                daily_usage_data['when_date'] if 'when_date' in daily_usage_data else 0.00,
+                daily_usage_data['others'] if 'others' in daily_usage_data else 0.00,
+                datetime.datetime.now(),
+                datetime.datetime.now(),
+            )
+        )
 
-select_cursor.execute('SELECT * FROM reports_unitnumber')
+if len(query_params) > 0:
+    db.set_usagedata(data=query_params)
 
-unit_numbers = select_cursor.fetchall()
-
-angaza.set_auth(username='atec_iot', password='U*p9fJi31$$X')
-
-for unit_number in unit_numbers:
-    print('Fetching data of unit number - ' + str(unit_number['unit_number']) + ' for ' + datetime.datetime.strftime(from_date, '%Y-%m-%d'))
-
-    select_cursor.execute('SELECT * FROM reports_usagedata WHERE unit_number = "'+ str(unit_number['unit_number']) +'" AND data_type = "228" ORDER BY when_datetime DESC LIMIT 1')
-
-    get_usage_data(
-        unit_number=unit_number['unit_number'],
-        from_when_dt='{}T01:00:00+00:00'.format(from_date)
-    )
-
-cursor = db.cursor()
-cursor.executemany(
-    query,
-    query_params
-)
-db.commit()
-
-print('Total ' + str(len(query_params)) + ' data was inserted')
+if len(daily_usage_data_query_params) > 0:
+    db.set_dailyusagedata(data=daily_usage_data_query_params)
--- seed_angaza.py
+++ seed_angaza.py
@@ -1,88 +1,21 @@
-import datetime, re, math
+import datetime
 from dateutil.relativedelta import relativedelta
-from mysql.connector import connect
-from angaza import Angaza
-
-def convert_to_datetime(item, format='%Y-%m-%dT%H:%M:%Sz'):
-    return datetime.datetime.strptime(item, format)
+from processes.db import Database
+from processes.helper import Helper
+from processes.vars import ANGAZA_DATA_TYPES
 
 
-def get_usage_data(unit_number: int, from_when_dt: str, to_when_dt: str=str(), offset: int = 0, variable = []):
-    data = angaza.get_usage_data(
-        unit_number=unit_number,
-        from_when_dt=from_when_dt,
-        offset=offset
-    )
-
-    if to_when_dt != '':
-        data = angaza.get_usage_data(
-            unit_number=unit_number,
-            from_when_dt=from_when_dt,
-            to_when_dt=to_when_dt,
-            offset=offset
-        )
-
-    if '_embedded' in data and len(data['_embedded']['item']) > 0:
-        variable += data['_embedded']['item']
-
-        if '_links' in data and 'next' in data['_links']:
-            link = data['_links']['next']['href']
-            offset = re.search('&offset=(.*)', link)
-
-            if offset and offset > 0:
-                if to_when_dt != '':
-                    get_usage_data(unit_number=unit_number, from_when_dt=from_when_dt, to_when_dt=to_when_dt, offset=offset, variable=variable)
-                else:
-                    get_usage_data(unit_number=unit_number, from_when_dt=from_when_dt, offset=offset, variable=variable)
-
-    return variable
-
-DB_HOST = 'localhost'
-DB_PORT = 3306
-DB_USER = 'root'
-DB_PASSWORD = 'root'
-DB_NAME = 'db_integration_angaza_data'
-
-TYPES = {
-    219: 'serial_number',
-    220: 'average_battery_voltage',
-    221: 'max_battery_voltage',
-    222: 'min_battery_voltage',
-    223: 'battery_critical_state_count',
-    224: '1_hour_samples_count',
-    225: 'lock_mode_entry_count',
-    226: 'factory_mode_entry_count',
-    227: 'gsm_sync_button_count',
-    228: 'stove_on_off_count',
-    229: 'daily_power_consumption',
-    230: 'left_stove_cooktime',
-    231: 'right_stove_cooktime',
-    232: 'others'
-}
 query_params = []
 daily_usage_data_query_params = []
+db = Database()
+helper = Helper()
 current_date = datetime.date.today()
 current_month = current_date.month
 current_year = current_date.year
 current_day = current_date.day
-angaza = Angaza()
-db = connect(
-    host=DB_HOST,
-    port=DB_PORT,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
-select_cursor = db.cursor(dictionary=True)
-query = 'INSERT INTO reports_usagedata (unit_number, when_datetime, data_type, data_value, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s)'
-daily_usage_data_query = 'INSERT INTO reports_dailyusagedata (serial_number, average_battery_voltage, max_battery_voltage, min_battery_voltage, battery_critical_state_count, one_hour_samples_count, lock_mode_entry_count, factory_mode_entry_count, gsm_sync_button_count, stove_on_off_count, daily_power_consumption, left_stove_cooktime, right_stove_cooktime, daily_cooking_time, average_power_consumption_per_use, average_cooking_time_per_use, when_date, others, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+unit_numbers = db.get_unitnumbers()
 
-select_cursor.execute('SELECT * FROM reports_unitnumber')
-
-unit_numbers = select_cursor.fetchall()
-
-angaza.set_auth(username='atec_iot', password='U*p9fJi31$$X')
-
+# Iterate all the unit numbers to get data against a unit number.
 for unit_number in unit_numbers:
     separator = str()
     end = False
@@ -95,11 +28,11 @@ for unit_number in unit_numbers:
 
     print(message)
 
+    # Fetch data from Angaza by 1 month till yesterday.
     while to_month.year <= current_year and to_month.date() <= current_date and not end:
         data = list()
         daily_usage_data_unorganized = list()
         daily_usage_data_without_stove_calculation = []
-        daily_usage_data_counter = 0
         done_type = []
         done_prev_usage_data = False
 
@@ -113,8 +46,9 @@ for unit_number in unit_numbers:
             to_month = datetime.datetime.today() - relativedelta(days=1)
             
         print('Running from ' + datetime.datetime.strftime(from_month, '%Y-%m-%d') + ' to ' + datetime.datetime.strftime(to_month, '%Y-%m-%d'))
-
-        get_usage_data(
+        
+        # Calling API to Angaza.
+        helper. get_usage_data(
             unit_number=unit_number['unit_number'],
             from_when_dt='{}T01:00:00+00:00'.format(datetime.datetime.strftime(from_month, '%Y-%m-%d')),
             to_when_dt='{}T01:00:00+00:00'.format(datetime.datetime.strftime(to_month, '%Y-%m-%d')),
@@ -128,9 +62,10 @@ for unit_number in unit_numbers:
                 data,
                 key=lambda item: item['when']
             )
-            
+
+            # Convert unreadable data from angaza to readable data.
             for item in data:
-                when_date = convert_to_datetime(item['when']).date()
+                when_date = helper.convert_to_datetime(item['when']).date()
 
                 if not (item['type'], when_date) in done_type:
                     done_type.append((item['type'], when_date))
@@ -138,14 +73,14 @@ for unit_number in unit_numbers:
                     temp_data = {
                         'when_date': str(when_date)
                     }
-                    temp_data[TYPES[item['type']]] = 0
+                    temp_data[ANGAZA_DATA_TYPES[item['type']]] = 0
 
                     for jitem in data:
-                        if item['type'] == jitem['type'] and when_date == convert_to_datetime(jitem['when']).date():
+                        if item['type'] == jitem['type'] and when_date == helper.convert_to_datetime(jitem['when']).date():
                             if item['type'] == 228:
-                                temp_data[TYPES[item['type']]] = jitem['value']
-                            elif when_date == convert_to_datetime(jitem['when']).date():
-                                temp_data[TYPES[item['type']]] += jitem['value']
+                                temp_data[ANGAZA_DATA_TYPES[item['type']]] = jitem['value']
+                            elif when_date == helper.convert_to_datetime(jitem['when']).date():
+                                temp_data[ANGAZA_DATA_TYPES[item['type']]] += jitem['value']
 
                     daily_usage_data_unorganized.append(temp_data)
 
@@ -154,6 +89,7 @@ for unit_number in unit_numbers:
             daily_usage_data = []
             stove_on_off_count = 0
 
+            # Get all data for the same date.
             for item in daily_usage_data_unorganized:
                 if not item['when_date'] in done_type:
                     done_type.append(item['when_date'])
@@ -169,7 +105,6 @@ for unit_number in unit_numbers:
                     daily_usage_data_without_stove_calculation.append(temp_data)
 
             # Stove on/off calculation.
-
             for item in daily_usage_data_without_stove_calculation:
                 if 'stove_on_off_count' in item:
                     temp = item['stove_on_off_count']
@@ -184,6 +119,7 @@ for unit_number in unit_numbers:
 
                 daily_usage_data.append(item)
 
+            # Prepare data to insert in to the DB.
             for item in data:
                 query_params.append(
                     (
@@ -245,39 +181,9 @@ for unit_number in unit_numbers:
     print(separator)
 
 if len(query_params) > 0:
-    cursor = db.cursor()
-    step = 25000
-    start_position = 0
-    end_position = step
-    
-    for _ in range(0, math.ceil(len(query_params) / step)):
-        temp_query_params = query_params[start_position:end_position]
-        
-        cursor.executemany(
-            query,
-            temp_query_params
-        )
-
-        start_position += step
-        end_position += step
-    db.commit()
+    db.set_usagedata(data=query_params)
 
 if len(daily_usage_data_query_params) > 0:
-    cursor = db.cursor()
-    step = 25000
-    start_position = 0
-    end_position = step
-    
-    for _ in range(0, math.ceil(len(daily_usage_data_query_params) / step)):
-        temp_query_params = daily_usage_data_query_params[start_position:end_position]
-        
-        cursor.executemany(
-            daily_usage_data_query,
-            temp_query_params
-        )
-
-        start_position += step
-        end_position += step
-    db.commit()
+    db.set_dailyusagedata(data=daily_usage_data_query_params)
 
 print('Total ' + str(len(query_params)) + ' data was inserted')

623b3bb2c01680d0c2c1a156f4f068ffa6344640
Hasibur Rahman Omi <hasib.omi@cimplux.com>
2021-03-15 15:49:09 +0600

    Map integration

--- reports/urls.py
+++ reports/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', ReportsView.as_view(), name='reports'),
     path('exports/', ExportView.as_view(), name='exports'),
     path('countries/', CountryListView.as_view(), name='countries_list'),
+    path('map/', MapView.as_view(), name='map'),
     path('countries/create/', CountryCreateView.as_view(), name='countries_create'),
     path('countries/<pk>/update/', CountryUpdateView.as_view(), name='countries_update'),
     path('countries/<pk>/delete/', CountryDeleteView.as_view(), name='countries_delete'),

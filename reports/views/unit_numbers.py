fc7b0bdca53ee33f1e42597b8b7f5b264098eb32
Hasibur Rahman Omi <hasib.omi@cimplux.com>
2021-03-22 17:42:04 +0600

    Import unit numbers from csv

--- reports/views/unit_numbers.py
+++ reports/views/unit_numbers.py
@@ -1,11 +1,15 @@
+import csv
+import codecs
 from typing import Any, Dict
 from django.db.models.query import QuerySet
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
-from reports.models import UnitNumber
+from reports.models import Country, UnitNumber
+from reports.forms import ImportForm
 
 
 class UnitNumberListView(LoginRequiredMixin, ListView):
@@ -57,3 +61,51 @@ class MapView(TemplateView):
         context['unit_numbers'] = UnitNumber.objects.all()
 
         return context
+
+
+class ImportView(TemplateView):
+    template_name = 'reports/unit_numbers/import.html'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ImportForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ImportForm(files=request.FILES)
+
+        if not form.is_valid():
+            return render(request, self.template_name, {
+                'form': form
+            })
+
+        file = form.cleaned_data['csv']
+        reader = csv.DictReader(codecs.iterdecode(file, 'utf-8'))
+        counter = 0
+
+        for row in reader:
+            country, created = Country.objects.get_or_create(
+                name=row['Country'],
+                defaults={
+                    'cost': 0.00
+                }
+            )
+            country = country.__dict__
+
+            UnitNumber.objects.update_or_create(
+                unit_number=row['Unit Number'],
+                defaults={
+                    'name': row['Name'],
+                    'country_id': country['id'],
+                    'description': row['Description'],
+                    'latitude': row['Latitude'] if row['Latitude'] != '' else 0.00,
+                    'longitude': row['Longitude'] if row['Longitude'] != '' else 0.00,
+                }
+            )
+
+            counter += 1
+
+        messages.success(request, f'Total {counter} row(s) has been inserted')
+
+        return redirect('reports:unit_numbers_list')
